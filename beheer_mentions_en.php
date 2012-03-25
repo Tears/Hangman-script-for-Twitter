facb9b5e1d34e59cd92d4eb402e755d8081474d3
Victor <victor@ubuntu.ubuntu-domain>
2012-03-25 18:59:27 +0200

    Updated the Dutch version and translated the Dutch version to an English version

--- beheer_mentions_en.php
+++ beheer_mentions_en.php
@@ -32,23 +32,24 @@ mysql_select_db("");
 date_default_timezone_set("Europe/Amsterdam");
 foreach($timeline as $mention)
 	{
-	echo "<br />hee. loop gestart <br />";
+	echo "<br />[0] LOOP STARTED<br />";
 	$speler_tegenstander = $mention->user->screen_name;
 	$mention_date = $mention->created_at;
 	mysql_query("SELECT * FROM `tmp_beheer` WHERE `type` = '0' AND `date` = '$mention_date'");
 	if(mysql_affected_rows() != 0)
 		{
-		echo "deze mention is al behandeld.<br />";
+		echo "<strong>[Error #01] This mention has already been processed and will be ingenored.</strong><br />";
 		}else{
 		mysql_query("INSERT INTO `tmp_beheer` (`type`, `date`) VALUES ('0', '$mention_date')");
 		$tweet->post('friendships/create', array('screen_name' => $speler_tegenstander));
 	//$speler_tegenstander = $_GET['speler_tegenstander'];
-	echo "mention is van $speler_tegenstander<br />";
+	# EDIT THE VARIABLE ABOVE TO TEST THIS GAME WITHOUT TWITTER
+	echo "[1] The author of this mention is $speler_tegenstander<br />";
 	$data = mysql_query("SELECT * FROM `potjesenwoorden` WHERE `tegen` = '$speler_tegenstander' AND `active` = '1'");
 	if(mysql_affected_rows() != 0)
 		{
 		
-		echo "oke er is een potje bezig<br />";
+		echo "[2] The author of this mention is in a game, so we will continue with the validation of the guess.<br />";
 		while($data2 = mysql_fetch_array($data))
 			{
 			$woord = $data2['woord'];
@@ -56,39 +57,40 @@ foreach($timeline as $mention)
 			$tegen = $data2['tegen'];
 			$van_speler = $data2['van'];
 			}
-			echo "woord: $woord, pogingen: $pogingen <br />";
+			echo "[3] The word to guess is $woord, and the opponent has $pogingen  attemts left.<br />";
 			$mention1 = $mention->text;
 			//$mention1 = $_GET['mention1'];
+			# EDIT THE VARIABEL ABOVE TO TEST THE GAME WITHOUT TWITTER
 			$last = $mention1[strlen($mention1)-1];
-			echo "laatste letter is {$last} <br />";
+			echo "[4] The last character of this mention is {$last} <br />";
 			$pos = strpos($woord, $last);
-			echo "laatste letter ({$last}) staat op de {$pos}e plaats<br />";
-			if(preg_match("/\bDit is het woord\b/i", $mention1))
+			echo "[5] The last character of this mention ({$last}) has the {$pos} position in this word.<br />";
+			if(preg_match("/\bThis is the word\b/i", $mention1))
 						{
-						echo "er wordt een woord geraden <br />";
+						echo "[6] The opponent is guessing the whole word.<br />";
 						$woordraw = explode("-", $mention1);
 						$woordgoed = trim($woordraw[1]);
-						echo "het gerade woord is $woordgoed <br />";
+						echo "[7] The opponent thinks that $woordgoed is the right word. <br />";
 						if($woordgoed == $woord)
 							{
-							echo "jeuj het woord is goed!<br />";
-							$tweetmsg = "@{$speler_tegenstander} correct! Je hebt gewonnen terwijl je nog {$pogingen} pogingen had! @{$van_speler} is de verliezer";
+							echo "[8] He was right, and he wins the game.<br />";
+							$tweetmsg = "@{$speler_tegenstander} Correct! You won with {$pogingen} attempts left! @{$van_speler} lost the game!";
 							$time_ended = date("G:i:s, d-m-Y");
 							mysql_query("UPDATE `potjesenwoorden` SET `active` = '0' AND `time_ended` = '$time_ended' WHERE `tegen` = '$speler_tegenstander'");
 							}else{
 							$pogingen_nu = --$pogingen;
 							$pogingen_lol = 10 - $pogingen_nu;
-							echo "haha kut voor je, woord niet goed. $pogingen_nu pogingen gehad dus nog $pogingen_lol over<br />";
+							echo "[7] Too bad for the opponent, he wasn't right. He had $pogingen_nu attempts and he now has $pogingen_lol left.<br />";
 							mysql_query("UPDATE `potjesenwoorden` SET `pogingen` = '$pogingen_nu' WHERE `tegen` = '$speler_tegenstander'");
-							$tweetmsg = "@{$speler_tegenstander} Helaas, dat is niet juist. Je hebt nog $pogingen_lol poging(en) over.";
+							$tweetmsg = "@{$speler_tegenstander} Too bad, $woordgoed wasn't right. You have $pogingen_lol attempt(s) left.";
 							}
 							}else{
 							if($found = strpos_andersom($woord, $last)) {
 							        foreach($found as $pos) {
 							        	$plaats = ++$pos;
-							            $tweetmsg1[] = "@{$speler_tegenstander} De letter $last zit in het woord op de {$plaats}e plaats! Goedzo (:";
+							            $tweetmsg1[] = "@{$speler_tegenstander} The character $last is on the {$plaats} place! Well done!";
 								         
-								echo "de letter staat op de goede plaats<br />";
+								echo "[6] Well done, opponent. The character $last is in the word on the $plaats place.<br />";
 									}
 								//$tweetmsg = "@{$speler_tegenstander} De letter $last zit in het woord op de $plaats plaats! Goedzo!";
 										
@@ -96,16 +98,16 @@ foreach($timeline as $mention)
 								if($pogingen == 1)
 									{
 									$gelukt = "nee";
-									echo "laatste poging mislukt ahahahaha loser<br />";
-									$tweetmsg = "@{$speler_tegenstander} {$last} is fout. Je hebt je pogingen verspeeld. Het woord was {$woord}. @{$van_speler} is de winnaar!";
+									echo "[6] Too bad for the opponent, the character wasn't right and it was his last attempt.<br />";
+									$tweetmsg = "@{$speler_tegenstander} {$last} is wrong. You have no attempts left. The word was {$woord}. @{$van_speler} is the winner!";
 									$time_ended = date("G:i:s, d-m-Y");
 									mysql_query("UPDATE `potjesenwoorden` SET `active` = '0' AND  `time_ended` = '$time_ended' WHERE `tegen` = '$speler_tegenstander'");
 									}else{
 								$pogingen_nu = --$pogingen;
 								$pogingen_lol = 10 - $pogingen_nu;
-								echo "haha kut voor je, letter niet goed. $pogingen_nu pogingen gehad dus nog $pogingen_lol over<br />";
+								echo "[6] Too bad for the opponent, he wasn't right. He had $pogingen_nu attempts and he now has $pogingen_lol left.<br />";
 								mysql_query("UPDATE `potjesenwoorden` SET `pogingen` = '$pogingen_nu' WHERE `tegen` = '$speler_tegenstander'");
-								$tweetmsg = "@{$speler_tegenstander} Helaas, de letter $last zit niet in het woord. Je hebt nog $pogingen_nu poging(en) over.";
+								$tweetmsg = "@{$speler_tegenstander} Too bad, the character $last is not in the word. You have $pogingen_nu attempt(s) left.";
 									}
 								}	
 						}
@@ -114,14 +116,14 @@ foreach($timeline as $mention)
 					foreach($tweetmsg1 as $meerdere_letters)
 						{
 						$tweet->post('statuses/update', array('status' => $meerdere_letters));
-						echo "tweet verzonden yeah<br />";
+						echo "[SUCCESS] The tweets with this information have been sent. We'll continue with the next tweet.<br />";
 						}
 					}else{
 					$tweet->post('statuses/update',array('status' => $tweetmsg));
-					echo "tweet verzonden jeuj<br />";
+					echo "[SUCCESS] The tweet with this information has been sent. We'll continue with the next tweet.<br />";
 					}
 		}else{
-		echo "niet in db doei<br/ >";
+		echo "[Error #02] The author of this mention is NOT playing a game right now, so we will ignore this tweet.<br/ >";
 		}
 		}
 	}		
